14467dea34d483f2798fabb5327e3ef6181b5115
Guilherme Ramalho <137944713+GuilhermePereiraRamalho@users.noreply.github.com>
2025-01-14 09:59:32 +0100

    Fazendo uso de lazy instance no padrao singleton

---
+++ secao03_singleton/lazy_singleton.py
@@ -0,0 +1,23 @@
+
+
+class Singleton:
+    __instance = None
+
+    def __init__(self):
+        if not Singleton.__instance:
+            print('O metodo __init__ foi chamado...')
+        else:
+            print(f'A instancia ja foi criada: {self.get_instance()}')
+
+    @classmethod
+    def get_instance(cls):
+        if not cls.__instance:
+            cls.__instance = Singleton()
+        return cls.__instance
+
+
+s1 = Singleton()
+
+print(f'Objeto criado agora: {Singleton.get_instance()}')
+
+s2 = Singleton()
